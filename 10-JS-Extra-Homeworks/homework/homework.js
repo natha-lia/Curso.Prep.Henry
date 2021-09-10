54a9f3e92a777b6dfe043d395e51ea73cb966a65
natha-lia <0413ntm@gmail.com>
2021-09-09 20:36:10 -0500

    homework solucionada

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -116,6 +116,15 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  let newArr = [] 
+  for(let i = 0; i < arreglo1.length; i++) {
+    for(let j = 0; j < arreglo2.length; j++) {
+      if(arreglo1[i] === arreglo2[j]) {
+        newArr.push(arreglo1[i])
+      }
+    }
+  }
+  return newArr
 }
 
 
